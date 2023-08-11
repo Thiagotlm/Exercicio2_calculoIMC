d4ee70b3da141bd5801a7e07afe36fad6d79d606
Thiago Mauricio <130397713+Thiagotlm@users.noreply.github.com>
2023-08-11 10:05:34 -0300

    resolução

---
+++ CálculoIMC.js
@@ -0,0 +1,30 @@
+/*O IMC - Índice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre a condição de peso de uma pessoa adulta.
+
+Fórmula di IMC:
+IMC = peso / (altura * altura)
+
+Elabore um algorítimo que dado o peso e a altura de um adulto, mostre sua condição de acordo com a tabela abaixo:
+
+IMC em adultos Condição:
+- Abaixo de 18.5 Abaixo do peso;
+- Entre 18.5 e 25 Peso normal;
+- Entre 25 e 30 Acima do peso;
+- Entre 30 e 40 Obeso;
+- Acima de 40 Obesidade Grave;*/
+
+let altura = 180;
+let peso = 80;
+const calcIMC = peso / (altura * altura);
+
+if (peso <18.5){
+    console.log('Abaixo do peso')
+}else if (peso >18.5 && peso <25){
+    console.log('Peso normal')
+}else if (peso >25 && peso <30){
+    console.log('Acima do peso')
+}else if (peso >30 && peso <40){
+    console.log('Obeso')
+}
+else {
+    console.log('Obesidade Grave')
+}
